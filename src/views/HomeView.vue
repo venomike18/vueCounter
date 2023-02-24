1193b9069bff7a385a42dfd09f2784362853501c
venomike18 <medas.michele@gmail.com>
2023-02-24 17:44:18 +0100

    add button

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,8 +3,10 @@
 <template>
   <main>
     <title>Random User Generator</title>
+    <img :class="gender" :src="picture" :alt="firstName"/>
     <h1>Hello {{firstName}} {{lastName}}</h1>
-    <h3>{{email }}</h3>
+    <h3>Email: {{email }}</h3>
+    <button :class="gender">Get Random User</button>
     <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
@@ -25,10 +27,6 @@
         picture: 'https://randomuser.me/api/portraits/men/10.jpg',
       }
     }
-
-
-
-
 
   }
 </script>
