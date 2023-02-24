30cf6d46aed20f538c2ed817c6f60c62c74219bd
venomike18 <medas.michele@gmail.com>
2023-02-24 17:28:41 +0100

    Hello World

---
+++ src/views/HomeView.vue
@@ -0,0 +1,34 @@
+
+
+<template>
+  <main>
+    <title>Random User Generator</title>
+    <h1>Hello {{firstName}} {{lastName}}</h1>
+    <h3>{{email }}</h3>
+    <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+    </nav>
+  </main>
+</template>
+
+
+<script>
+  export default {
+    name: "HomeView",
+    data(){
+      return{
+        firstName: 'Jhon',
+        lastName: 'Doe',
+        email: 'john@gmail.com',
+        gender: 'male',
+        picture: 'https://randomuser.me/api/portraits/men/10.jpg',
+      }
+    }
+
+
+
+
+
+  }
+</script>
