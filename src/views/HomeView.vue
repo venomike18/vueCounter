f952a9ed64567b363e25059a21a8c689f53ce759
venomike18 <medas.michele@gmail.com>
2023-03-01 16:25:54 +0100

    Update integrate API

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,7 +6,8 @@
     <img :class="gender" :src="picture" :alt="firstName"/>
     <h1>Hello {{firstName}} {{lastName}}</h1>
     <h3>Email: {{email }}</h3>
-    <button :class="gender">Get Random User</button>
+    <button v-on:click="getUser()" :class="gender">Random User</button>
+    <button v-on:click="changeSex()" :class="gender">Change Sex</button>
     <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
@@ -26,7 +27,29 @@
         gender: 'male',
         picture: 'https://randomuser.me/api/portraits/men/10.jpg',
       }
-    }
+    },
+    methods:{
+      async getUser(){
+        const res = await fetch('https://randomuser.me/api')
+        const { results } = await res.json()
 
+        this.firstName = results[0].name.first
+        this.lastName = results[0].name.last
+        this.email = results[0].email
+        this.gender = results[0].gender
+        this.picture = results[0].picture.large
+      },
+      changeSex(){
+        if(this.gender== 'male'){
+          this.gender = 'female'
+          this.firstName = 'Maria'
+          this.picture = 'https://randomuser.me/api/portraits/women/10.jpg'
+        }else if(this.gender== 'female'){
+          this.gender ='male'
+          this.firstName = 'Mario'
+          this.picture = 'https://randomuser.me/api/portraits/men/10.jpg'
+        }
+      },
+    },
   }
 </script>
